83bc035b4d83d57267c14ac62dee68ec49f053a7
Charles Marshall <charles@oneblackbear.com>
2013-02-05 20:22:43 +0000

    no space

--- ini.php
+++ ini.php
@@ -1,5 +1,4 @@
 <?
 AutoLoader::register_assets("stylesheets/wildfire.rss",__DIR__."/resources/public/stylesheets/wildfire.rss/", "/*.css");
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
-
 ?>
